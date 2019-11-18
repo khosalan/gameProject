5a0bf47c65aa851bb5fe4f0cd36ab72b929230ad
Yathu <2017cs198@stu.ucsc.cmb.ac.lk>
2019-11-18 19:11:24 +0530

    Did Scenario Canvas

---
+++ Assets/ImageZoom.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageZoom : MonoBehaviour
+{    
+
+    public void OnMouseEnter(Image image)
+    {
+        image.rectTransform.localScale = new Vector3(2,2);        
+    }
+
+    public void OnMouseExit(Image image)
+    {
+        image.rectTransform.localScale = new Vector3(1, 1);        
+    }
+
+    public void ShowText(GameObject text)
+    {
+        text.SetActive(true);
+    }
+
+    public void HideText(GameObject text)
+    {
+        text.SetActive(false);
+    }
+}
