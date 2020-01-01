93303d35b5530ac26f4a8fc0793a87864588b52e
Yathu <2017cs198@stu.ucsc.cmb.ac.lk>
2020-01-01 15:55:57 +0530

    Started Intro and level selection

---
+++ Assets/Intro.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Intro : MonoBehaviour
+{
+    public GameObject loginCanvas;
+    public GameObject introCanvas;
+    public void hideIntro()
+    {
+        loginCanvas.SetActive(true);
+        introCanvas.SetActive(false);
+    }
+}
