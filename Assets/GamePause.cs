c815751e95bd6ffc834bafc1611b6bdf2f3f1ff6
Yathu <2017cs198@stu.ucsc.cmb.ac.lk>
2020-01-06 12:07:40 +0530

    LevelCanvas Edited

--- Assets/GamePause.cs
+++ Assets/GamePause.cs
@@ -4,8 +4,32 @@ using UnityEngine;
 
 public class GamePause : MonoBehaviour
 {
+    public InkStoryManager inkStoryManager;
+    private bool _isPaused = false;
+
     public void OnClickPauseButton()
     {
-        gameObject.SetActive(true);
+        if(!_isPaused)
+        {
+            Time.timeScale = 0; //game paused
+            _isPaused = true;
+            gameObject.SetActive(true);
+        }
+    }
+
+    public void OnClickResume()
+    {
+        if(_isPaused)
+        {
+            Time.timeScale = 1;
+            _isPaused = false;
+            gameObject.SetActive(false);
+        }
+    }
+
+    public void OnClickRestart()
+    {
+        OnClickResume();
+        inkStoryManager.StartStory();
     }
 }
